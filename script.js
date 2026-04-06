44ed2f12836c22038c4dc18dbb9d3ecf2aafa0cc
Aman <amanbhai36yt@gmail.com>
2026-04-06 20:11:50 +0530

    feature added

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// code UPdate soon..
+const hour = document.querySelector("#hour");
+const min = document.querySelector("#minute");
+const sec = document.querySelector("#second");
+const amPm = document.querySelector("#ampm");
+
+function updateTime(){
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    if(h>12){
+        h = h - 12;
+        amPm.innerText = "PM"
+    }
+    h = h<10 ? "0" + h : h ;
+    m = m<10 ? "0" + m : m ;
+    s = s<10 ? "0" + s : s ;
+    hour.innerText = h;
+    min.innerText = m;
+    sec.innerText = s;
+    setTimeout(()=>{
+        updateTime();
+    },1000);
+}
+updateTime();
